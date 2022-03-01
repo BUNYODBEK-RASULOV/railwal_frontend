645e6c64babc8dc2ad1fb84f29c8c2ae7b740123
BUNYODBEK-RASULOV <7777777tegir@gmail.com>
2022-03-01 12:28:07 +0500

    boilerplate

---
+++ src/main/java/uz/mafia/railway_frontend/RailwayFrontendApplication.java
@@ -0,0 +1,13 @@
+package uz.mafia.railway_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/mafia/railway_frontend/RailwayFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mafia.railway_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
